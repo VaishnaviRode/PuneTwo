8b7cd69eca678ba725d9a46802aac103a554946b
VaishnaviRode <vaishnavirode18@gmail.com>
2023-09-10 11:02:18 +0530

    hi

--- Hello.java
+++ Hello.java
@@ -2,7 +2,5 @@ class Hello{
     public static void main(String[] args) {
         System.out.println("hello");
          System.out.println("pune");
-          System.out.println("V");
-          System.out.println("Vaishnavi");
     }
 }
